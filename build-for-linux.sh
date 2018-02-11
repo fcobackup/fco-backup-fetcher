651bc0c5352e3a0cb2274387f286d058966650b2
FCO Backup <ukfcobackup@gmail.com>
2018-02-11 11:38:48 +0000

    build-for-linux.sh works from other directories

--- build-for-linux.sh
+++ build-for-linux.sh
@@ -2,9 +2,9 @@
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-image="$(docker build docker/build | awk '$0 ~ /^Successfully built / {print $3}')"
+image="$(docker build "${script_dir}/docker/build" | awk '$0 ~ /^Successfully built / {print $3}')"
 name="$(uuidgen)"
 docker run --name="${name}" -v "${script_dir}:/src" -w /src "${image}" cargo build --release
-docker cp "${name}:/src/target/release/fco-backup-fetcher" docker/run/fco-backup-fetcher
+docker cp "${name}:/src/target/release/fco-backup-fetcher" "${script_dir}/docker/run/fco-backup-fetcher"
 echo >&2 "Stopping build container: $(docker stop "${name}" || :)"
 echo >&2 "Removing build container $(docker rm "${name}")"
